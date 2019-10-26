2950003d6ecc8106fd529da65463d772c3c44a0e
Jmay <justin.sean.may@gmail.com>
2019-10-26 16:01:27 -0400

    created react app

---
+++ web-app/src/components/Game.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type GameProps = {
+}
+
+type GameState = {
+}
+class Game extends React.Component<GameProps,GameState> {
+    render() {
+      return <h1>Hello</h1>;
+    }
+}
+
+export default Game;
+

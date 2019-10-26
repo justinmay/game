23054e98c751048e25e2463d0d68639c07406291
Jmay <justin.sean.may@gmail.com>
2019-10-26 16:38:38 -0400

    changes

--- web-app/src/components/Game.tsx
+++ web-app/src/components/Game.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import Cell from './Cell';
+import '../stylesheets/Game.css';
+
+type cell = {
+    terrain: string,
+}
 
 type GameProps = {
 }
 
 type GameState = {
+    board: cell[][],
 }
 
 class Game extends React.Component<GameProps,GameState> {
+    constructor(props: GameProps) {
+        super(props)
+        this.state={
+            board: Array(40).fill(0).map(() => {return new Array(this.state !== undefined? this.state.size: 40).fill(cell)})
+        }
+    }
     render() {
-      return <h1>Hello <Cell/></h1>;
+      return(
+          <div className="GridContainer">
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+              <Cell/>
+          </div>
+      );
     }
 }
 
